3b745183e9256856332922f41eca4d2b0a638361
rascencioc <rascencio@crabi.com>
2019-02-27 22:41:21 -0600

    End of countdown message and higlight current day

--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -3,12 +3,20 @@ import Radium from 'radium';
 
 import './Countdown.css'
 
-const countdown = (props) => {  
-  return <div className="divCountDown">
-    <div className="box_time"><p>{props.days + ''}</p> dias</div>
-    <div className="box_time"><p>{props.hours + ''}</p> hrs</div>
-    <div className="box_time"><p>{props.minutes + ''}</p> min</div>
-    <div className="box_time"><p>{props.seconds + ''}</p> sec</div>
+const countdown = (props) => {
+  if (props.boxStyle === 'joy_text'){
+    return <div className={props.style}>
+    <div className={props.boxStyle}><p>{props.days + ' '}</p></div>
+    <div className={props.boxStyle}><p>{props.hours + ' '}</p></div>
+    <div className={props.boxStyle}><p>{props.minutes + ' '}</p></div>
+    <div className={props.boxStyle}><p>{props.seconds + ' '}</p></div>
+  </div>
+  }
+  return <div className={props.style}>
+    <div className={props.boxStyle}><p>{props.days + ' '}</p> dias</div>
+    <div className={props.boxStyle}><p>{props.hours + ' '}</p> hrs</div>
+    <div className={props.boxStyle}><p>{props.minutes + ' '}</p> min</div>
+    <div className={props.boxStyle}><p>{props.seconds + ' '}</p> sec</div>
   </div>
 };
 
--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -7,44 +7,44 @@ const config = require('config');
 
 class Day extends Component {
 
-  // componentDidMount(){
-  //   let date = new Date();
-  //   let cityID = 0;
-  //   if(date.getDate() < 18){
-  //     cityID = process.env.ID_LOS_ANGELES || config.get('ID_LOS_ANGELES');
-  //   }else {
-  //     cityID = process.env.ID_ANAHEIM || config.get('ID_ANAHEIM');
-  //   }
-  // }
-  //
-  // getWeather = async () => {
-  //   const invokeUrl = process.env.WEATHER_URL || config.get('WEATHER_URL'),
-  //   const response = await fetch(invokeUrl+'?id=');
-  //   const body = await response.json();
-  //   if (response.status !== 200)
-  //     throw Error(body.message);
-  //     return body;
-  // }
-  //
-  // getForecast = async () => {
-  //
+   //componentDidMount(){
+
+     /* let cityID = 0;
+     if(date.getDate() < 18){
+       cityID = process.env.ID_LOS_ANGELES || config.get('ID_LOS_ANGELES');
+     }else {
+       cityID = process.env.ID_ANAHEIM || config.get('ID_ANAHEIM');
+     } */
    //}
+  
+   /* getWeather = async () => {
+     const invokeUrl = process.env.WEATHER_URL || config.get('WEATHER_URL'),
+     const response = await fetch(invokeUrl+'?id=');
+     const body = await response.json();
+     if (response.status !== 200)
+       throw Error(body.message);
+       return body;
+   }
+  
+   getForecast = async () => {
+  
+   } */
 
 
 
   render () {
-    return (
-      <div className="Day">
+    
+    return (      
+      <div className={this.props.style}>
               <div>
                 <div>
-                <h2>{this.props.date}&nbsp;-&nbsp;{this.props.main}</h2>
-                <h4>14/24°</h4>
+                <h2>April {this.props.date}&nbsp;-&nbsp;{this.props.main}</h2>                
                 </div>
               </div>
               <details>
                 <summary>Actividades</summary>
                 <br/>
-                {this.props.activities.map((activity, index) => {
+                {this.props.activities.map((activity, index) => {                  
                    return <Activity
                     time={activity.time}
                     name={activity.name}
