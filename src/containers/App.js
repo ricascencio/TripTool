815a98c34f2487e1b0c0b3fba8f8a2cc847afe36
ricascencio <ricardo.ascencio@gmail.com>
2019-03-11 21:42:36 -0600

    Update flight's time

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ class App extends Component {
        { id: '5', date: 'Wednesday 17', main: 'California Adventure', activities: [ {id: '1', time: '08:00 - 22:00', name: 'California Adventure'}]},
        { id: '6', date: 'Thursday 18', main: 'Hollywood', activities: [ {id: '1', time: '08:00 - 20:00', name: 'PENDIENTE',}]},
        { id: '7', date: 'Friday 19', main: 'Universal Studios', activities: [ {id: '1', time: '08:00 - 09:00', name: 'Desayuno'}, {id: '2', time: '10:00 - 19:00', name: 'Universal Studios'}, {id: '3', time: '19:00 - 20:00', name: 'Cenar'}]},
-       { id: '8', date: 'Saturday 20', main: 'Regreso', activities: [ {id: '1', time: '15:00', name: 'Vuelo'}]}
+       { id: '8', date: 'Saturday 20', main: 'Regreso', activities: [ {id: '1', time: '16:04', name: 'Vuelo'}]}
     ],
     countdown: {
         days: 0,
@@ -24,7 +24,7 @@ class App extends Component {
         seconds: 0,
         targetDate: '04/13/2019',        
         targetHours: '06',
-        targetMinutes: '37'
+        targetMinutes: '12'
     },
     intervalId : 0
   }
